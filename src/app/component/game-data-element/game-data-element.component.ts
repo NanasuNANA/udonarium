45db49a1838664a3d96bcc6609d01728a6a81371
Nanasu NANA <nnanasu7@gmail.com>
2018-04-02 00:59:34 +0900

    共有メモのフォントサイズもスライダーに。

--- src/app/component/game-data-element/game-data-element.component.ts
+++ src/app/component/game-data-element/game-data-element.component.ts
@@ -27,7 +27,8 @@ export class GameDataElementComponent implements OnInit, OnDestroy, AfterViewIni
       return this.isTagLocked && (this.gameDataElement.name === 'size'
         || this.gameDataElement.name === 'width'
         || this.gameDataElement.name === 'height'
-        || this.gameDataElement.name === 'depth');
+        || this.gameDataElement.name === 'depth'
+        || this.gameDataElement.name === 'fontsize');
     }
     return false;
   }
